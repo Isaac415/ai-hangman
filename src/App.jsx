85a526d7d5c9565b04ceff2e664b7e62c6dc82b0
Isaac415 <isaac.cheng97@gmail.com>
2025-04-02 18:38:45 +0800

    Add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Game from './components/Game.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
   return (
       <main>
           <Game mode={"robot"} />
+          <Analytics />
       </main>
   )
 }
