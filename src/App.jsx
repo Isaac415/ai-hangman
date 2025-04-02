e1b0bddc8977400bd3485bded586f87c769557b0
Isaac415 <isaac.cheng97@gmail.com>
2025-04-02 18:44:59 +0800

    Remove Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import Game from './components/Game.jsx'
-import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
   return (
       <main>
           <Game mode={"robot"} />
-          <Analytics />
       </main>
   )
 }
